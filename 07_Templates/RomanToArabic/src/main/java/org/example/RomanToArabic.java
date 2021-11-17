086e136be15860aa63721b58afcfa76b71831aa6
abinder2005 <alexbinder01@gmail.com>
2021-11-17 13:24:47 +0100

    IV test is true

--- 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -4,9 +4,20 @@ public class RomanToArabic
 {
     public static int toArabic(String romanNumber) {
         int arabicNumber = 0;
+        boolean four = true;
+        boolean one = true;
+        boolean five = true;
         for (int i = 0; i < romanNumber.length(); i++) {
-            if (romanNumber.charAt(i) == 'I') arabicNumber += 1;
-            if (romanNumber.charAt(i) == 'V') arabicNumber += 5;
+            if (romanNumber.length() >= 2 && romanNumber.charAt(romanNumber.length()-1) == 'V'
+                    && romanNumber.charAt(romanNumber.length()-2) == 'I' && four) {
+                arabicNumber += 4;
+                one = false;
+                five = false;
+                four = false;
+            }
+
+            if (romanNumber.charAt(i) == 'I' && one) arabicNumber += 1;
+            if (romanNumber.charAt(i) == 'V' && five) arabicNumber += 5;
             if (romanNumber.charAt(i) == 'X') arabicNumber += 10;
             if (romanNumber.charAt(i) == 'L') arabicNumber += 50;
             if (romanNumber.charAt(i) == 'C') arabicNumber += 100;
