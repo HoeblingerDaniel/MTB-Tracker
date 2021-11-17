ecac3de9921d375b0aede3233cce6de0eac8a99c
abinder2005 <alexbinder01@gmail.com>
2021-11-17 11:01:56 +0100

    third test is true

--- 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -3,6 +3,7 @@ package org.example;
 public class RomanToArabic
 {
     public static int toArabic(String romanNumber) {
+        if (romanNumber == "III") return 3;
         if (romanNumber == "II") return 2;
         return 1;
     }
