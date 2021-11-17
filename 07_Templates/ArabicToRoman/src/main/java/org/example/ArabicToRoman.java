f3b67c3a6055ca1a7218c6a0ad84b47fba675f84
abinder2005 <alexbinder01@gmail.com>
2021-11-17 10:42:57 +0100

    created project ArabicToRoman

---
+++ 07_Templates/ArabicToRoman/src/main/java/org/example/ArabicToRoman.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArabicToRoman
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
