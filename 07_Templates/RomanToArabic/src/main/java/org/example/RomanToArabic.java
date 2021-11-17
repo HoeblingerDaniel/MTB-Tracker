80f1a5e18eeecaf07102bca1bc2d5f83c867e6e8
abinder2005 <alexbinder01@gmail.com>
2021-11-17 10:48:03 +0100

    changed names

---
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RomanToArabic
+{
+    public static int toArabic(String romanNumber) {
+        return 0;
+    }
+}
---
+++ 07_Templates/RomanToArabic/src/test/java/org/example/RomanToArabicTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RomanToArabicTest
+{
+    @Test
+    public void itShouldReturn1_GivenI() {
+        
+    }
+}
