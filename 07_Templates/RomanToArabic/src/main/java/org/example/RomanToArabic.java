c766ccd6fe956b81616828f4e051c4a86bfcb462
abinder2005 <alexbinder01@gmail.com>
2021-11-17 12:42:43 +0100

    eighth

--- 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -4,9 +4,10 @@ public class RomanToArabic
 {
     public static int toArabic(String romanNumber) {
         int arabicNumber = 0;
-        if (romanNumber.charAt(0) == 'V') arabicNumber += 5;
         for (int i = 0; i < romanNumber.length(); i++) {
             if (romanNumber.charAt(i) == 'I') arabicNumber += 1;
+            if (romanNumber.charAt(i) == 'V') arabicNumber += 5;
+            if (romanNumber.charAt(i) == 'X') arabicNumber += 10;
         }
         return arabicNumber;
     }
