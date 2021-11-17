cb270a43b4dd71318621aea88ca05ca3b2bd4bed
abinder2005 <alexbinder01@gmail.com>
2021-11-17 12:37:52 +0100

    fifth test is true

--- 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -3,7 +3,11 @@ package org.example;
 public class RomanToArabic
 {
     public static int toArabic(String romanNumber) {
-        if (romanNumber == "V") return 5;
-        return romanNumber.length();
+        int arabicNumber = 0;
+        if (romanNumber.charAt(0) == 'V') arabicNumber += 5;
+        for (int i = 0; i < romanNumber.length(); i++) {
+            if (romanNumber.charAt(i) == 'I') arabicNumber += 1;
+        }
+        return arabicNumber;
     }
 }
