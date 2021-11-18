47537a347130a54457689fbf5d9424b8406d5d2c
maximilianhaas2005@gmail.com <maximilianhaas2005@gmail.com>
2021-11-18 22:35:34 +0100

    true

--- 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
+++ 07_Templates/RomanToArabic/src/main/java/org/example/RomanToArabic.java
@@ -4,10 +4,11 @@ public class RomanToArabic
 {
     public static int toArabic(String romanNumber) {
         int arabicNumber = 0;
-        boolean condition = true;
-        boolean condition9 = true;
-        boolean condition400 = true;
         boolean condition4 = true;
+        boolean condition40 = true;
+        boolean condition400 = true;
+
+        boolean condition9 = true;
         boolean condition90 = true;
         boolean condition900 = true;
 
@@ -33,10 +34,22 @@ public class RomanToArabic
                 condition9 = false;
             }
 
+            if (index("CM", romanNumber)  != -1 && condition900) {
+                arabicNumber += 900;
+                condition900 = false;
+            }
+
+            if (index("XL", romanNumber)  != -1 && condition40) {
+                arabicNumber += 40;
+                condition40 = false;
+            }
+
+
+
 
             if (romanNumber.charAt(i) == 'I' && condition4 && condition9) arabicNumber += 1;
             if (romanNumber.charAt(i) == 'V' && condition4) arabicNumber += 5;
-            if (romanNumber.charAt(i) == 'X' && condition9 && condition90 ) arabicNumber += 10;
+            if (romanNumber.charAt(i) == 'X' && condition9 && condition90 && condition40 ) arabicNumber += 10;
             if (romanNumber.charAt(i) == 'L') arabicNumber += 50;
             if (romanNumber.charAt(i) == 'C'&& condition400 && condition90) arabicNumber += 100;
             if (romanNumber.charAt(i) == 'D'&& condition400 && condition900) arabicNumber += 500;
