12a32713d9c32c8ee19f6ea3f594d1bd545f7de6
Alex <alexbinder01@gmail.com>
2022-03-11 08:23:39 +0100

    created App Project

---
+++ 03_SourceCode/MTBTracker_SmartphoneApp/app/src/main/java/at/htlleonding/mtb_tracker_smartphoneapp/MainActivity.java
@@ -0,0 +1,14 @@
+package at.htlleonding.mtb_tracker_smartphoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
