608dc795220d10593d0a6a0f12b18272e51192dd
maxhaas1 <maximilianhaas2005@gmail.com>
2022-05-20 11:15:46 +0200

    GPS

---
+++ 03_SourceCode/MTBTracker_SmartphoneApp/app/src/main/java/at/htlleonding/mtb_tracker_smartphoneapp/activity/ActivityMapsBinding.java
@@ -0,0 +1,7 @@
+package at.htlleonding.mtb_tracker_smartphoneapp.activity;
+
+public class ActivityMapsBinding {
+    public int getRoot() {
+        return 0;
+    }
+}
--- 03_SourceCode/MTBTracker_SmartphoneApp/app/src/main/java/at/htlleonding/mtb_tracker_smartphoneapp/activity/MainActivity.java
+++ 03_SourceCode/MTBTracker_SmartphoneApp/app/src/main/java/at/htlleonding/mtb_tracker_smartphoneapp/activity/MainActivity.java
@@ -1,28 +1,8 @@
 package at.htlleonding.mtb_tracker_smartphoneapp.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.Button;
-
-import at.htlleonding.mtb_tracker_smartphoneapp.R;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button addTrackButton = findViewById(R.id.addTrackButton);
-        addTrackButton.setOnClickListener(v -> {
-           // Intent addTrackIntent = new Intent(getBaseContext(), AddTrailActivity.class);
-          //  startActivity(addTrackIntent);
-            setContentView(R.layout.activity_add_track);
-        });
+public class MainActivity {
+    public static void main(String[] args) {
+        GPSTracker gpsTracker = new GPSTracker();
+        System.out.println();
     }
-
 }
-
-
-//https://www.programmierenlernenhq.de/tutorial-android-daten-in-datei-speichern/
